e465a7d9a63630220c7c7a83ebc57d5243f573b8
Amol Dharmadhikari <adharmad@ionate.io>
2020-11-22 22:11:04 -0800

    Changes

---
+++ src/main/java/com/example/javaexamples/string/CharOps.java
@@ -0,0 +1,15 @@
+package com.example.javaexamples.string;
+
+public class CharOps {
+    public static void main(String[] args) {
+        String s = "hello";
+
+        char c = s.charAt(1);
+        System.out.println(c);
+        System.out.println((int)c - (int)'a');
+
+        int i = 8;
+
+        System.out.println((char)8);
+    }
+}
---
+++ src/main/java/com/example/javaexamples/string/SBufOps.java
@@ -0,0 +1,8 @@
+package com.example.javaexamples.string;
+
+public class SBufOps {
+    public static void main(String[] args) {
+        StringBuffer sbuf = new StringBuffer();
+        System.out.println(sbuf.length());
+    }
+}
