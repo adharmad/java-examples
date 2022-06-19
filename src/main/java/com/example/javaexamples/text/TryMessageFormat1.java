5f3063f16123c3e8bead1cab8532b36c2e16ec35
Amol Dharmadhikari <adharmad@ionate.io>
2022-06-18 17:29:00 -0700

    Java MessageFormat example

--- src/main/java/com/example/javaexamples/text/TryMessageFormat1.java
+++ src/main/java/com/example/javaexamples/text/TryMessageFormat1.java
@@ -1,2 +1,15 @@
-package com.example.javaexamples.text;public class TryMessageFormat1 {
+package com.example.javaexamples.text;
+
+import java.text.FieldPosition;
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.List;
+
+public class TryMessageFormat1 {
+    public static void main(String[] args) {
+        String pattern = "---------------------------&#xD;&#xA;{0} &#xD;&#xA;---------------------------&#xD;&#xA;{1}&#xD;&#xA;{2} ";
+        pattern = pattern.replace("&#xD;&#xA;", System.getProperty("line.separator"));
+        String text = MessageFormat.format(pattern, new Date().toString(), "var1", "java.Lang.Exception");
+        System.out.println(text);
+    }
 }
