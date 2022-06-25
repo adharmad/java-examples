c6f6c28a1009be71db4d100f8a6d2b4cafeeb80a
Amol Dharmadhikari <adharmad@ionate.io>
2022-06-24 17:29:00 -0700

    StringTokenizer example

--- src/main/java/com/example/javaexamples/tok/Strtok1.java
+++ src/main/java/com/example/javaexamples/tok/Strtok1.java
@@ -1,2 +1,34 @@
-package com.example.javaexamples.tok;public class Strtok1 {
+package com.example.javaexamples.tok;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Strtok1 {
+    public static void main(String[] args) {
+        String pattern1 = "---------------------------&#xD;&#xA;${CURRENT_TIMESTAMP} &#xD;&#xA;---------------------------&#xD;&#xA;${Environment.Variables}&#xD;&#xA;${ExceptionList} ";
+        String[] split = pattern1.split("\\$\\{|\\}");
+        System.out.println("hello");
+
+        List<String> patterns = new ArrayList<>();
+        List<String> overall = new ArrayList<>();
+        for (int i=0 ; i<split.length ; i++) {
+            if (i%2 == 0) {
+                overall.add(split[i]);
+            } else {
+                patterns.add(split[i]);
+            }
+        }
+
+        StringBuffer sbuf = new StringBuffer();
+        for (int i=0 ; i<overall.size() ; i++) {
+            sbuf.append(overall.get(i));
+            if (i< overall.size()-1) {
+                sbuf.append("{" + i + "}");
+            }
+        }
+
+        System.out.println(patterns);
+        System.out.println(sbuf.toString());
+    }
 }
