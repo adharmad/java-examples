bfbf2b7dd481fd3c9ebc43c7b0b1d83a3a9c037f
Amol Dharmadhikari <adharmad@ionate.io>
2022-06-25 17:29:00 -0700

    apache StrSubstitutor example

--- src/main/java/com/example/javaexamples/apache/TryStrSub.java
+++ src/main/java/com/example/javaexamples/apache/TryStrSub.java
@@ -1,2 +1,24 @@
-package com.example.javaexamples.apache;public class TryStrSub {
+package com.example.javaexamples.apache;
+
+import org.apache.commons.lang.text.StrSubstitutor;
+
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.Map;
+
+public class TryStrSub {
+    public static void main(String[] args) {
+        String pattern = "---------------------------&#xD;&#xA;${foo} &#xD;&#xA;---------------------------&#xD;&#xA;${bar}&#xD;&#xA;${baz} ";
+        pattern = pattern.replace("&#xD;&#xA;", System.getProperty("line.separator"));
+
+        Map<String, String> map = Map.of(
+                "foo", "hello",
+                "bar", "world",
+                "baz", "asdasd"
+        ) ;
+
+        StrSubstitutor strSubstitutor = new StrSubstitutor(map);
+        String text = strSubstitutor.replace(pattern);
+        System.out.println(text);
+    }
 }
